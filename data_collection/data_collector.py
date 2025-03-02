5e92e0675cf908c6ade624ee1a0dc1de94784a70
Mattia Greiche <167469181+mattiagreiche@users.noreply.github.com>
2025-03-01 23:37:50 -0500

    Add methods to DataCollector for easier data collection
    Change the DataCollector overall "output" (the return value of the get_data method) to 8x8x12 numpy arrays, which are 8x8 chess boards with 12 channels for each chess piece (also separated by color). Sight refactor likely needed.

--- data_collection/data_collector.py
+++ data_collection/data_collector.py
@@ -1,6 +1,7 @@
 import requests
 
 import pandas as pd
+import numpy as np
 
 import chess.pgn
 import io
@@ -47,6 +48,8 @@ class DataCollector():
             exit()
 
         months = archives_response.json().get("archives", [])
+        
+        months.reverse() # Reverse the list so that the most recent months are first
 
         all_games = []  # Initialize an empty list to collect all games
     
@@ -59,6 +62,8 @@ class DataCollector():
             month_data = month_response.json()
             games = month_data.get("games", [])
             all_games.extend(games)  # Add games to the list
+            if len(all_games) > 10_000:
+                break # Stop after collecting 10,000 games
 
         # Return the games as a pandas DataFrame
         return pd.DataFrame(all_games)
@@ -72,34 +77,7 @@ class DataCollector():
         """
         games = self.fetch_games()
         return games['pgn']
-
-    def pgn_to_san(self, pgn: str) -> list[str]:
-        """
-        Converts a PGN to Standard Algebraic Notation (SAN).
-
-        Args:
-            pgn (str): The PGN to be converted.
-
-        Returns:
-            list[str]: A list of SAN moves extracted from the PGN.
-        """
-        # Parse the PGN using python-chess
-        pgn_io = io.StringIO(pgn)
-        game = chess.pgn.read_game(pgn_io)
-
-        # Initialize an empty list to hold moves
-        moves = []
-
-        # Iterate through the moves of the main line
-        board = game.board()
-        for move in game.mainline_moves():
-            # Convert the move to Standard Algebraic Notation (SAN)
-           san = board.san(move)
-           moves.append(san)
-           board.push(move)
-        
-        return moves
-
+    
     def get_user_color(self, pgn: str) -> str:
         """
         Determines the color (white or black) of the user in a game.
@@ -120,36 +98,197 @@ class DataCollector():
         else:
             return "black"
 
-    def get_san_and_color(self) -> tuple[list, str]:
+    def pgn_to_boards(self, pgn: str) -> list[chess.Board]:
         """
-        Returns a list of tuples containing the SAN moves and user color for each game.
+        Converts a PGN to a list of chess.Board objects for each game state in the PGN (only on user turns). 
+
+        Args:
+            pgn (str): The PGN to be converted.
 
         Returns:
-            tuple[list, str]: A tuple containing a list of SAN moves and the user color for each game.
+            list[chess.Board]: A list of chess.Board objects extracted from the PGN. The chess.Board objects only contain game states for which it is the user's turn.
         """
-        all_sans_and_colors = []
-        
+        # Parse the PGN using python-chess
+        pgn_io = io.StringIO(pgn)
+        game = chess.pgn.read_game(pgn_io)
+
+        # Initialize an empty list to hold boards
+        boards = []
+
+        # Iterate through the moves of the main line
+        board = game.board()
+        user_color = self.get_user_color(pgn)
+        if user_color == 'white':
+            user_starting_move = 0
+        else:
+            user_starting_move = 1
+        for move_number, move in enumerate(game.mainline_moves()):
+            if move_number % 2 == user_starting_move: # Only add the board if it is the user's turn to move. Works by checking if move number is even if user is white, odd if user is black.
+                if user_color == 'white':
+                    boards.append(board.copy())
+                else:
+                    boards.append(board.mirror().copy()) # Mirror the board if user is black, so that resulting matrices are consistent whether white or black.
+            board.push(move)
+            
+        return boards
+    
+    def get_boards(self) -> list[list[chess.Board]]:
+        """
+        Retrieves a list of chess boards from all available PGN files, with one board per game state where it is the user's turn.
+
+        Returns:
+            A list of lists, where each inner list represents the chess boards extracted from a single PGN file (single game).
+        """
+        all_boards = []
         all_pgns = self.get_all_pgns()
         for pgn in all_pgns:
-            san = self.pgn_to_san(pgn)
-            user_color = self.get_user_color(pgn)
-            all_sans_and_colors.append((san, user_color))
-        return all_sans_and_colors
-
-    def get_all_game_states(self) -> list[str]:
+            if type(pgn) != str: # Sometimes, the Chess.com API gives a None value or float value for a game, so we need to skip it
+                continue
+            all_boards.append(self.pgn_to_boards(pgn))
+        return all_boards
+            
+    def piece_to_int(self, piece: chess.Piece) -> int:
         """
-        Returns a list of game states at each move for all games played by the user.
+        Converts a chess piece object to an integer representation.
+
+        Args:
+            piece (chess.Piece): The chess piece object to be converted.
 
         Returns:
-            list[str]: A list of game states at each move for all games played by the user.
+            int: The integer representation of the chess piece.
+            No piece: 0
+            Pawn: 1
+            Knight: 2
+            Bishop: 3
+            Rook: 4
+            Queen: 5
+            King: 6
         """
-        all_game_states = []
-        for san, user_color in self.get_san_and_color():
-            if user_color == 'white':
-                first_user_move = 0
-            else:
-                first_user_move = 1
-            for user_move in range(len(san) // 2):
-                current_game_state = san[:first_user_move + 2 * user_move]
-                all_game_states.append(current_game_state)
-        return all_game_states
+        piece_to_int = {
+            None: 0,
+            chess.PAWN: 1,
+            chess.KNIGHT: 2,
+            chess.BISHOP: 3,
+            chess.ROOK: 4,
+            chess.QUEEN: 5,
+            chess.KING: 6
+        }
+        return piece_to_int[piece.piece_type]
+    
+    def get_matrix_game_states(self) -> list[list[np.ndarray]]:
+        """
+        Converts the board game states into matrices representing the positions of the chess pieces.
+
+        Returns:
+            A list of lists of numpy arrays, where each numpy array in the inner list represents a game state as a matrix, and each outer list is one game.
+        """
+        all_board_game_states = self.get_boards()
+        
+        all_matrices = []
+        i = 0
+        for game in all_board_game_states:
+            game_matrices = []
+            for board in game:
+                matrix = np.zeros((8, 8), dtype=int)
+                for square in range(64):
+                    piece = board.piece_at(square)
+                    if piece is None:
+                        continue
+                    row, col = square // 8, square % 8
+                    value = self.piece_to_int(piece)
+                    if piece.color == chess.BLACK: # Although this seems like it switches black pieces to negative, it is actually switching the perspective of the board so that the user's pieces are always positive.
+                        value *= -1
+                    matrix[7 - row][col] = value
+
+                game_matrices.append(matrix)
+            all_matrices.append(game_matrices)
+        return all_matrices
+    
+    def find_moved_piece(self, board_state_1: np.ndarray, board_state_2: np.ndarray) -> tuple[int, int] | None:
+        """
+        Finds the moved piece between two board states.
+
+        Args:
+            board_state_1 (np.ndarray): The initial board state.
+            board_state_2 (np.ndarray): The next board state.
+
+        Returns:
+            None if no piece was moved.
+            tuple[int, int]: A tuple containing the square number (0 to 63) and the value of the piece that was moved.
+            Pawn: 1
+            Knight: 2
+            Bishop: 3
+            Rook: 4
+            Queen: 5
+            King: 6
+        """
+        flattened_board_state_1 = board_state_1.flatten()
+        flattened_board_state_2 = board_state_2.flatten()
+        
+        exists_moved_piece = False
+        
+        for square_number, piece_in_square in enumerate(flattened_board_state_1):
+            if piece_in_square != flattened_board_state_2[square_number] and piece_in_square > 0: # If the piece in the square is different between the two board states and the piece is positive (user's piece)
+                moved_square_number = square_number
+                moved_piece = piece_in_square
+                exists_moved_piece = True
+        if exists_moved_piece:
+            return moved_square_number, moved_piece
+        return None
+    
+
+    def board_to_8x8x12(self, game_state: np.ndarray) -> np.ndarray:
+        """
+        Convert an 8x8 board matrix to an 8x8x12 representation.
+
+        The board matrix should use:
+          0 for empty squares,
+          1 for white pawn, 2 for white knight, 3 for white bishop, 
+          4 for white rook, 5 for white queen, 6 for white king,
+         -1 for black pawn, -2 for black knight, -3 for black bishop,
+         -4 for black rook, -5 for black queen, -6 for black king.
+     
+        Returns:
+            A NumPy array of shape (8, 8, 12) with binary indicators.
+        """
+        # Initialize the 8x8x12 array with zeros
+        board_12 = np.zeros((8, 8, 12), dtype=np.int8)
+    
+        # Loop over each square in the 8x8 board
+        for i in range(8):
+            for j in range(8):
+                piece = game_state[i, j]
+                if piece == 0:
+                    continue  # Skip empty squares
+                # For white pieces (positive values), channel = piece value - 1 (0-indexed)
+                if piece > 0:
+                    channel = int(piece) - 1
+                # For black pieces (negative values), channel = (abs(piece) - 1) + 6
+                else:
+                    channel = int(abs(piece)) - 1 + 6
+                board_12[i, j, channel] = 1
+
+        # Transpose from (8,8,12) to (12,8,8)
+        board_12 = np.transpose(board_12, (2, 0, 1))
+        return board_12
+
+    
+    def get_data(self) -> list[tuple[list[np.ndarray], int]]:
+        data = []
+        
+        matrix_game_states = self.get_matrix_game_states()
+        
+        for individual_game_states in matrix_game_states: # individual_game_states are all matrix game states for a single game
+            for move_number, game_state in enumerate(individual_game_states): 
+                if move_number == len(individual_game_states) - 1:
+                    break # If move_number is the last available game_state of the game, break the loop and move to next game
+                board_state_1 = game_state
+                board_state_2 = individual_game_states[move_number + 1]
+                try:
+                    moved_square_number, moved_piece = self.find_moved_piece(board_state_1=board_state_1, board_state_2=board_state_2)
+                except TypeError:
+                    continue
+                game_state_12 = self.board_to_8x8x12(game_state)
+                
+                data.append((game_state_12, moved_square_number))
+        return data
